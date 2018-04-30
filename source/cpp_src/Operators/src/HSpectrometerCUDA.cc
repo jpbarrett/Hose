ae03b220d8aab1714bda0f45e2039820a7ca4293
John Barrett <barrettj@mit.edu>
2018-04-30 10:19:17 -0400

    cant used scoped lock in c++11, unique lock instead

--- source/cpp_src/Operators/src/HSpectrometerCUDA.cc
+++ source/cpp_src/Operators/src/HSpectrometerCUDA.cc
@@ -49,45 +49,45 @@ HSpectrometerCUDA::ExecuteThreadTask()
     {
         //first get a sink buffer from the buffer handler
         HProducerBufferPolicyCode sink_code = this->fSinkBufferHandler.ReserveBuffer(this->fSinkBufferPool, sink);
+
         if(sink_code == HProducerBufferPolicyCode::success)
         {
             std::cout<<"got a sink buffer"<<std::endl;
+            std::unique_lock<std::mutex> sink_lock(sink->fMutex, std::defer_lock);
+
             //now grab a source buffer
             HConsumerBufferPolicyCode source_code = this->fSourceBufferHandler.ReserveBuffer(this->fSourceBufferPool, source);
             if(source_code == HConsumerBufferPolicyCode::success)
             {
-                if(source != nullptr && sink != nullptr)
-                {
-                    std::cout<<"got a source and sink buffer"<<std::endl;
-                    //lock dem buffers
-                    std::scoped_lock<std::mutex> local_lock(sink->fMutex, source->fMutex);
-                    // std::lock_guard<std::mutex> sink_lock(sink->fMutex);
-                    // std::lock_guard<std::mutex> source_lock(source->fMutex);
+                std::unique_lock<std::mutex> source_lock(source->fMutex, std::defer_lock);
 
-                    // //calculate the noise rms (may eventually need to move this calculation to the GPU)
-                    fPowerCalc.SetBuffer(source);
-                    fPowerCalc.Calculate();
+                std::cout<<"got a source and sink buffer"<<std::endl;
+                //lock dem buffers
+                std::lock(sink_lock, source_lock);
 
-                    //point the sdata to the buffer object (this is a horrible hack)
-                    sdata = &( (sink->GetData())[0] ); //should have buffer size of 1
+                // //calculate the noise rms (may eventually need to move this calculation to the GPU)
+                fPowerCalc.SetBuffer(source);
+                fPowerCalc.Calculate();
 
-                    //set meta data
-                    *( sink->GetMetaData() ) = *( source->GetMetaData() );
-                    sdata->sample_rate = source->GetMetaData()->GetSampleRate();
-                    sdata->acquistion_start_second = source->GetMetaData()->GetAcquisitionStartSecond();
-                    sdata->leading_sample_index = source->GetMetaData()->GetLeadingSampleIndex();
-                    sdata->data_length = source->GetArrayDimension(0); //also equal to fSpectrumLength*fNAverages;
-                    sdata->spectrum_length = fSpectrumLength;
-                    sdata->n_spectra = fNAverages;
+                //point the sdata to the buffer object (this is a horrible hack)
+                sdata = &( (sink->GetData())[0] ); //should have buffer size of 1
 
-                    //call Juha's process_vector routine
-                    process_vector_no_output(source->GetData(), sdata);
-                    std::cout<<"processed on gpu"<<std::endl;
+                //set meta data
+                *( sink->GetMetaData() ) = *( source->GetMetaData() );
+                sdata->sample_rate = source->GetMetaData()->GetSampleRate();
+                sdata->acquistion_start_second = source->GetMetaData()->GetAcquisitionStartSecond();
+                sdata->leading_sample_index = source->GetMetaData()->GetLeadingSampleIndex();
+                sdata->data_length = source->GetArrayDimension(0); //also equal to fSpectrumLength*fNAverages;
+                sdata->spectrum_length = fSpectrumLength;
+                sdata->n_spectra = fNAverages;
 
-                    //release the buffers
-                    this->fSourceBufferHandler.ReleaseBufferToProducer(this->fSourceBufferPool, source);
-                    this->fSinkBufferHandler.ReleaseBufferToConsumer(this->fSinkBufferPool, sink);
-                }
+                //call Juha's process_vector routine
+                process_vector_no_output(source->GetData(), sdata);
+                std::cout<<"processed on gpu"<<std::endl;
+
+                //release the buffers
+                this->fSourceBufferHandler.ReleaseBufferToProducer(this->fSourceBufferPool, source);
+                this->fSinkBufferHandler.ReleaseBufferToConsumer(this->fSinkBufferPool, sink);
             }
         }
 
