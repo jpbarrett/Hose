6be565a82ef13cf178efcd2f93e55df265d84278
John Barrett <barrettj@mit.edu>
2018-04-30 10:26:34 -0400

    double check null

--- source/cpp_src/Operators/src/HSpectrometerCUDA.cc
+++ source/cpp_src/Operators/src/HSpectrometerCUDA.cc
@@ -59,35 +59,38 @@ HSpectrometerCUDA::ExecuteThreadTask()
             HConsumerBufferPolicyCode source_code = this->fSourceBufferHandler.ReserveBuffer(this->fSourceBufferPool, source);
             if(source_code == HConsumerBufferPolicyCode::success)
             {
-                std::unique_lock<std::mutex> source_lock(source->fMutex, std::defer_lock);
+                if(source != nullptr && sink != nullptr)
+                {
+                    std::unique_lock<std::mutex> source_lock(source->fMutex, std::defer_lock);
 
-                std::cout<<"got a source and sink buffer"<<std::endl;
-                //lock dem buffers
-                std::lock(sink_lock, source_lock);
+                    std::cout<<"got a source and sink buffer"<<std::endl;
+                    //lock dem buffers
+                    std::lock(sink_lock, source_lock);
 
-                // //calculate the noise rms (may eventually need to move this calculation to the GPU)
-                fPowerCalc.SetBuffer(source);
-                fPowerCalc.Calculate();
+                    // //calculate the noise rms (may eventually need to move this calculation to the GPU)
+                    fPowerCalc.SetBuffer(source);
+                    fPowerCalc.Calculate();
 
-                //point the sdata to the buffer object (this is a horrible hack)
-                sdata = &( (sink->GetData())[0] ); //should have buffer size of 1
+                    //point the sdata to the buffer object (this is a horrible hack)
+                    sdata = &( (sink->GetData())[0] ); //should have buffer size of 1
 
-                //set meta data
-                *( sink->GetMetaData() ) = *( source->GetMetaData() );
-                sdata->sample_rate = source->GetMetaData()->GetSampleRate();
-                sdata->acquistion_start_second = source->GetMetaData()->GetAcquisitionStartSecond();
-                sdata->leading_sample_index = source->GetMetaData()->GetLeadingSampleIndex();
-                sdata->data_length = source->GetArrayDimension(0); //also equal to fSpectrumLength*fNAverages;
-                sdata->spectrum_length = fSpectrumLength;
-                sdata->n_spectra = fNAverages;
+                    //set meta data
+                    *( sink->GetMetaData() ) = *( source->GetMetaData() );
+                    sdata->sample_rate = source->GetMetaData()->GetSampleRate();
+                    sdata->acquistion_start_second = source->GetMetaData()->GetAcquisitionStartSecond();
+                    sdata->leading_sample_index = source->GetMetaData()->GetLeadingSampleIndex();
+                    sdata->data_length = source->GetArrayDimension(0); //also equal to fSpectrumLength*fNAverages;
+                    sdata->spectrum_length = fSpectrumLength;
+                    sdata->n_spectra = fNAverages;
 
-                //call Juha's process_vector routine
-                process_vector_no_output(source->GetData(), sdata);
-                std::cout<<"processed on gpu"<<std::endl;
+                    //call Juha's process_vector routine
+                    process_vector_no_output(source->GetData(), sdata);
+                    std::cout<<"processed on gpu"<<std::endl;
 
-                //release the buffers
-                this->fSourceBufferHandler.ReleaseBufferToProducer(this->fSourceBufferPool, source);
-                this->fSinkBufferHandler.ReleaseBufferToConsumer(this->fSinkBufferPool, sink);
+                    //release the buffers
+                    this->fSourceBufferHandler.ReleaseBufferToProducer(this->fSourceBufferPool, source);
+                    this->fSinkBufferHandler.ReleaseBufferToConsumer(this->fSinkBufferPool, sink);
+                }
             }
         }
 
