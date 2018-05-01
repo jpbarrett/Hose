db0f40bc81459a77026b61cefbc6bcc8b8c715f4
John Barrett <barrettj@mit.edu>
2018-05-01 14:04:43 -0400

    disable write

--- source/cpp_src/Operators/src/HSimpleMultiThreadedSpectrumDataWriter.cc
+++ source/cpp_src/Operators/src/HSimpleMultiThreadedSpectrumDataWriter.cc
@@ -36,50 +36,46 @@ HSimpleMultiThreadedSpectrumDataWriter::ExecuteThreadTask()
         if(buffer_code & HConsumerBufferPolicyCode::success && tail != nullptr)
         {
 
-            std::unique_lock<std::mutex> lock(tail->fMutex, std::try_to_lock);
-            if(lock.owns_lock())
+            std::lock_guard<std::mutex> lock(tail->fMutex)
+            //initialize the thread workspace
+            spectrometer_data* sdata = nullptr;
+
+            //get sdata pointer
+            sdata = &( (tail->GetData())[0] ); //should have buffer size of 1
+
+            if(sdata != nullptr)
             {
-                //initialize the thread workspace
-                spectrometer_data* sdata = nullptr;
+                //we rely on acquisitions start time and sample index to uniquely name/stamp a file
+                std::stringstream ss;
+                ss << fOutputDirectory;
+                ss << "/";
+                ss <<  sdata->acquistion_start_second;
+                ss << "_";
+                ss <<  sdata->leading_sample_index;
+                ss << ".bin";
 
-                //get sdata pointer
-                sdata = &( (tail->GetData())[0] ); //should have buffer size of 1
+                //std::cout<<"file name = "<<ss.str()<<std::endl;
+                // std::cout<<"size of on accumulations = "<<tail->GetMetaData()->GetOnAccumulations()->size()<<std::endl;
+                // std::cout<<"size of off accumulations = "<<tail->GetMetaData()->GetOffAccumulations()->size()<<std::endl;
 
-                if(sdata != nullptr)
-                {
-                    //we rely on acquisitions start time and sample index to uniquely name/stamp a file
-                    std::stringstream ss;
-                    ss << fOutputDirectory;
-                    ss << "/";
-                    ss <<  sdata->acquistion_start_second;
-                    ss << "_";
-                    ss <<  sdata->leading_sample_index;
-                    ss << ".bin";
-
-                    //std::cout<<"file name = "<<ss.str()<<std::endl;
-                    // std::cout<<"size of on accumulations = "<<tail->GetMetaData()->GetOnAccumulations()->size()<<std::endl;
-                    // std::cout<<"size of off accumulations = "<<tail->GetMetaData()->GetOffAccumulations()->size()<<std::endl;
-
-                    HSpectrumObject< float > spec_data;
-                    spec_data.SetStartTime( sdata->acquistion_start_second );
-                    spec_data.SetSampleRate( sdata->sample_rate );
-                    spec_data.SetLeadingSampleIndex(  sdata->leading_sample_index );
-                    spec_data.SetSampleLength( (sdata->n_spectra)*(sdata->spectrum_length)  );
-                    spec_data.SetNAverages( sdata->n_spectra );
-                    spec_data.SetSpectrumLength((sdata->spectrum_length)/2+1); //Fix naming of this
-                    spec_data.SetSpectrumData(sdata->spectrum);
-                    spec_data.ExtendOnAccumulation( tail->GetMetaData()->GetOnAccumulations() );
-                    spec_data.ExtendOffAccumulation( tail->GetMetaData()->GetOffAccumulations() );
-
-                    spec_data.WriteToFile(ss.str());
-                    spec_data.ReleaseSpectrumData();
-
-                    lock.unlock();
-                
-                    //free the tail for re-use
-                    this->fBufferHandler.ReleaseBufferToProducer(this->fBufferPool, tail);
-                }
+                // HSpectrumObject< float > spec_data;
+                // spec_data.SetStartTime( sdata->acquistion_start_second );
+                // spec_data.SetSampleRate( sdata->sample_rate );
+                // spec_data.SetLeadingSampleIndex(  sdata->leading_sample_index );
+                // spec_data.SetSampleLength( (sdata->n_spectra)*(sdata->spectrum_length)  );
+                // spec_data.SetNAverages( sdata->n_spectra );
+                // spec_data.SetSpectrumLength((sdata->spectrum_length)/2+1); //Fix naming of this
+                // spec_data.SetSpectrumData(sdata->spectrum);
+                // spec_data.ExtendOnAccumulation( tail->GetMetaData()->GetOnAccumulations() );
+                // spec_data.ExtendOffAccumulation( tail->GetMetaData()->GetOffAccumulations() );
+                // 
+                // spec_data.WriteToFile(ss.str());
+                // spec_data.ReleaseSpectrumData();
+                // 
+                //free the tail for re-use
+                this->fBufferHandler.ReleaseBufferToProducer(this->fBufferPool, tail);
             }
+
 
         }
     }
