b7167fb957aca9f0cdcd7ff3f8af7cbc94d5e591
John Barrett <barrettj@mit.edu>
2018-05-01 08:37:33 -0400

    debugh

--- source/cpp_src/Operators/include/HPeriodicPowerCalculator.hh
+++ source/cpp_src/Operators/include/HPeriodicPowerCalculator.hh
@@ -219,11 +219,11 @@ class HPeriodicPowerCalculator
                 }
             }
 
-            if(on_intervals.size() > 1 || off_intervals.size() > 1 || ( on_intervals.size() == 0 && off_intervals.size() == 0) )
-            {
+            // if(on_intervals.size() > 1 || off_intervals.size() > 1 || ( on_intervals.size() == 0 && off_intervals.size() == 0) )
+            // {
                 std::cout<<"n on intervals = "<<on_intervals.size()<<std::endl;
                 std::cout<<"n off intervals = "<<off_intervals.size()<<std::endl;
-            }
+            // }
 
         }
 
