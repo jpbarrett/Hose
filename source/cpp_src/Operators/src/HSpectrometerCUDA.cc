e2ca25a67a039b9598b83b92dbda62c237c61cea
John Barrett <barrettj@mit.edu>
2018-05-02 10:46:19 -0400

    renable noise stat

--- source/cpp_src/Operators/src/HSpectrometerCUDA.cc
+++ source/cpp_src/Operators/src/HSpectrometerCUDA.cc
@@ -66,15 +66,15 @@ HSpectrometerCUDA::ExecuteThreadTask()
                 std::lock_guard<std::mutex> source_lock(source->fMutex);
                 //std::cout<<"thread: "<< std::this_thread::get_id()<<" locking source: "<<source<< "\n";
 
-                // //calculate the noise rms (may eventually need to move this calculation to the GPU)
-                // HPeriodicPowerCalculator< uint16_t > powerCalc;
-                // 
-                // powerCalc.SetSamplingFrequency(fSamplingFrequency);
-                // powerCalc.SetSwitchingFrequency(fSwitchingFrequency);
-                // powerCalc.SetBlankingPeriod(fBlankingPeriod);
-                // 
-                // powerCalc.SetBuffer(source);
-                // powerCalc.Calculate();
+                //calculate the noise rms (may eventually need to move this calculation to the GPU)
+                HPeriodicPowerCalculator< uint16_t > powerCalc;
+                
+                powerCalc.SetSamplingFrequency(fSamplingFrequency);
+                powerCalc.SetSwitchingFrequency(fSwitchingFrequency);
+                powerCalc.SetBlankingPeriod(fBlankingPeriod);
+                
+                powerCalc.SetBuffer(source);
+                powerCalc.Calculate();
 
                 //point the sdata to the buffer object (this is a horrible hack)
                 sdata = &( (sink->GetData())[0] ); //should have buffer size of 1
