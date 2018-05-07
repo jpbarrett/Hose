683379f25a937852149fb8170d6606f60bf9bf1a
John Barrett <barrettj@mit.edu>
2018-05-07 08:37:46 -0400

    try again

--- source/cpp_src/Test/TestMultiThreadPX14Spectrometer.cc
+++ source/cpp_src/Test/TestMultiThreadPX14Spectrometer.cc
@@ -55,7 +55,7 @@ int main(int /*argc*/, char** /*argv*/)
     sdata_alloc->SetSpectrumLength(FAKE_SPECTRUM_LENGTH);
 
     HBufferPool< spectrometer_data >* sink_pool = new HBufferPool< spectrometer_data >( sdata_alloc );
-    const size_t sink_n_chunks = 32;
+    const size_t sink_n_chunks = 24;
     const size_t sink_items_per_chunk = 1; //THERE CAN BE ONLY ONE!!!
     sink_pool->Allocate(sink_n_chunks, sink_items_per_chunk);
     std::cout<<"done"<<std::endl;
