0f1a3d2045af098074d9dd8ac9f78cd626e8eebd
John Barrett <barrettj@mit.edu>
2018-06-04 11:33:26 -0400

    reinserting short to float conversion, not sure how that got commented out

--- source/cpp_src/CUDASpectrometer/src/spectrometer.cu
+++ source/cpp_src/CUDASpectrometer/src/spectrometer.cu
@@ -434,7 +434,7 @@ extern "C" void process_vector_no_output(uint16_t *d_in, spectrometer_data *d)
     }
 
     // // convert datatype using GPU
-    // short_to_float<<< N_BLOCKS_S, N_THREADS_S >>>(d->ds_in, d->d_in, d->d_window, n_spectra, spectrum_length);
+    short_to_float<<< N_BLOCKS_S, N_THREADS_S >>>(d->ds_in, d->d_in, d->d_window, n_spectra, spectrum_length);
 
     // cufft kernel execution
     if (cufftExecR2C(d->plan, (float *)d->d_in, (cufftComplex *)d->d_z_out)
