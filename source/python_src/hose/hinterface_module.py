9a9436272513c6386a7b3fd68dd4a951ec6d0fd1
John Barrett <barrettj@mit.edu>
2018-06-05 14:40:26 -0400

    some comments

--- source/python_src/hose/hinterface_module.py
+++ source/python_src/hose/hinterface_module.py
@@ -79,6 +79,7 @@ class spectrum_file_data(hose_structure_base):
 
     #access to the spectrum data should be done through this function
     #not from the raw_spectrum_data with is a raw char array
+    #TODO, make this more robust in the presence of other formats (check the version_flag!)
     def get_spectrum_data(self):
         #have to convert the raw char array to float
         fmt = '@'
