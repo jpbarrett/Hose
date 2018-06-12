470b8f5b6ad78f9cfd8223c44c1e173af28fc91d
John Barrett <barrettj@mit.edu>
2018-06-12 11:14:23 -0400

    add daemon

---
+++ source/cpp_src/Main/LaunchSpectrometerDaemon.cc
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+
+#include "HSpectrometerManager.hh"
+
+static void daemonize(void)
+{
+    pid_t pid, sid;
+    int fd; 
+
+    //already running a child process
+    if ( getppid() == 1 ) return;
+
+    pid = fork();
+    if (pid < 0){exit(1); } // could not fork
+    if (pid > 0){exit(0); } // kill parent
+
+    //promote child to its own process
+    sid = setsid();
+    if (sid < 0){exit(1);}
+
+    //change working directory to HOSE bin install
+    if( chdir( STR2(BIN_INSTALL_DIR) ) < 0){exit(1);}
+
+    //redirect stdout/stderr/etc to /dev/null
+    fd = open("/dev/null",O_RDWR, 0);
+    if (fd != -1)
+    {
+        dup2(fd, STDIN_FILENO);
+        dup2(fd, STDOUT_FILENO);
+        dup2(fd, STDERR_FILENO);
+        if (fd > 2){close (fd);}
+    }
+
+    /*resettign File Creation Mask */
+    umask(022);
+}
+
+
+int main(int /*argc*/, char** /*argv*/)
+{
+    daemonize();
+
+    /* this thread runs now runs as a daemon in the background */
+    HSpectrometerManager* specManager = new HSpectrometerManager();
+    specManager->Initialize();
+    std::thread spectrometer_thread( &HSpectrometerManager::Run, specManager);
+    spectrometer_thread.join();
+
+    return 0;
+}
