940a83272c0c4f063606b7e34d64ff8b86718dcb
John Barrett <barrettj@mit.edu>
2018-06-18 15:56:14 -0400

    working on fs log parsing

--- source/python_src/hose/hfslog_module.py
+++ source/python_src/hose/hfslog_module.py
@@ -4,6 +4,8 @@ from datetime import datetime, timedelta
 class time_stamp(object):
 
     def __init__(self):
+        self.date_string_length = 20
+        self.valid = False
         self.year = 0
         self.day = 0
         self.hour = 0
@@ -11,209 +13,171 @@ class time_stamp(object):
         self.seconds = 0
         self.microseconds = 0
 
+    #strip the date code from a log line, return time stamp object
+    def initialize_from_line(self, line):
+        self.valid = False
+        if(len(line) >= date_string_length ):
+            self.valid = True
+            date_string = line[0:self.date_string_length]
+            #date string should be formatted like 2018.060.15:05:26.36
+            syear = date_string[0:4]
+            sday = date_string[5:8]
+            shour = date_string[9:11]
+            sminute = date_string[12:14]
+            ssec = date_string[15:17]
+            shundredths = date_string[18:20]
+            if syear.isdigit() and sday.isdigit() and shour.isdigit() and sminute.isdigit() and ssec.isdigit() and shundredths.isdigit():
+                self.year = int(syear)
+                self.day = int(sday)
+                self.hour = int(shour)
+                self.minute = int(sminute)
+                self.seconds = int(ssec)
+                self.microseconds = int(shundredths)*10000
+                if year > 3000:
+                    self.valid = False
+                if day < 1 or day > 366:
+                    self.valid = False
+                if hour > 24:
+                    self.valid = False
+                if minute > 59:
+                    self.valid = False
+                if ssec > 59:
+                    self.valid = False
+                if microseconds > 1000000:
+                    self.valid = False
+            else:
+                self.valid = False
+        return self.valid
+
+
     def get_formatted_utc(self):
         abstime = datetime(self.year,1,1) + timedelta(days=self.day-1, hours=self.hour, minutes=self.minute, seconds=self.seconds, microseconds = self.microseconds)
         return abstime.strftime('%Y-%m-%dT%H:%M:%S.%fZ')
 
-class udc_status(object):
+
+class fs_log_line(object):
 
     def __init__(self):
-        self.log_time = time_stamp() 
+        self.log_time = time_stamp() #every log line must have a time stamp
         self.parse_valid = False
-        self.name = 'c'
-        self.frequency_MHz = 0.0
-        self.atten_h = 0.0
-        self.atten_v = 0.0
-        self.status_code = 0
+        self.line_key = "none" #line must contain this to trigger parsing
+        self.name = "none" #name given to this data object (measurement) in the database
+        self.data_fields = dict() #dictionary of data fields
+        self.token_map = dict() #map token indices on to data fields 
+        self.primary_delim = ""
+        self.secondary_delim = ""
+
+    def initialize_from_line(self, line):
+        self.parse_valid = True
+        self.log_time = time_stamp()
+        if( self.line_key in line):
+            success = self.log_time.initialize_from_line(line)
+            if success is True:
+                primary_split = line.split(self.primary_delim)
+                if len(primary_split) == 2:
+                    payload = primary_split[1].strip()
+                    tokens = payload.split(self.secondary_delim)
+                    for key,val in token_map:
+                        if key < len(tokens):
+                            self.data_fields[val] = tokens[key]
+                else:
+                    self.parse_valid = False
+                    return
+            else:
+                self.parse_valid = False
+                return
+            self.init_hook()
+        else:
+            self.parse_valid = False
+        return self.parse_valid
+
+    def init_hook(self):
+        #provide interface for sub-class to modify data_fields, but do nothing by default
 
     def as_dict(self):
         point_value = {
                 "time": self.log_time.get_formatted_utc(),
-                "measurement": "udc_status",
-                "fields": {
-                    "frequency_MHz": self.frequency_MHz,
-                    "attenuation_h": self.atten_h,
-                    "attenuation_v": self.atten_v,
-                    "udc": self.name
-                }
+                "measurement": self.name,
+                "fields": self.data_fields
             }
         return point_value
 
-class data_status(object):
 
+#currently we only care about UDC-C
+#we expect the line to have the following format: 2018.061.13:12:04.39#popen#udccc/updown 7083.1 20 20 0 0 status 0
+class udc_status(fs_log_line):
     def __init__(self):
-        self.log_time = time_stamp() 
-        self.parse_valid = False
-        self.status_code = 0
+        super(udc_status, self).__init__()
+        self.line_key = "udccc/updown"
+        self.name = "udc_status"
+        self.data_fields = {"frequency_MHz": 0, "attenuation_h": 0, "attenuation_v": 0, "udc": "c"}
+        self.token_map = {0: "frequency_MHz", 1: "attenuation_h", 2: "attenuation_v"}
+        self.primary_delim = "udccc/updown"
+        self.secondary_delim = " "
 
-    def as_dict(self):
-        point_value = {
-                "time": self.log_time.get_formatted_utc(),
-                "measurement": "data_validity",
-                "fields": {
-                    "status": self.status_code
-                }
-            }
-        return point_value
+    def init_hook(self):
+        self.data_fields["frequency_MHz"] = float(self.data_fields["frequency_MHz"])
+        self.data_fields["attenuation_h"] = float(self.data_fields["attenuation_h"])
+        self.data_fields["attenuation_v"] = float(self.data_fields["attenuation_v"])
 
+#2018.166.18:44:41.29#flagr#flagr/antenna,acquired
+#2018.166.18:24:45.00#flagr#flagr/antenna,new-source
+#2018.166.18:45:29.56#flagr#flagr/antenna,off-source
+#2018.166.18:45:46.57#flagr#flagr/antenna,re-acquired
+class antenna_target_status(fs_log_line):
+    def __init__(self):
+        super(antenna_target_status, self).__init__()
+        self.line_key = "flagr/antenna"
+        self.name = "antenna_target_status"
+        self.data_fields = {"status": "", "acquired": ""}
+        self.token_map = {1: "status"}
+        self.primary_delim = "flagr/antenna"
+        self.secondary_delim = ","
+
+    def init_hook(self):
+        if "acquired" in self.data_fields["status"]:
+            self.data_fields["acquired"] = "yes"
+        else:
+            self.data_fields["acquired"] = "no"
+
+#2018.166.18:24:44.00&casa/source=casa,232324.8,+584859.,2000.
 class source_status(object):
-
     def __init__(self):
-        self.log_time = time_stamp() 
-        self.parse_valid = False
-        self.source_name = ""
-        self.ra = 0
-        self.dec = 0
-        self.epoch = 0
-        self.cable_wrap = ""
+        super(source, self).__init__()
+        self.line_key = "source="
+        self.name = "source_status"
+        self.data_fields = {"source": "", "ra": "", "dec": "", "epoch" : "" }
+        self.token_map = {0 : "source", 1: "ra", 2: "dec", 3: "epoch"}
+        self.primary_delim = "source="
+        self.secondary_delim = ","
 
-    def as_dict(self):
-        point_value = {
-                "time": self.log_time.get_formatted_utc(),
-                "measurement": "source",
-                "fields": {
-                    "ra": self.ra,
-                    "dec": self.dec,
-                    "epoch": self.epoch,
-                    "cable_wrap": self.cable_wrap,
-                    "source_name": self.source_name
-                }
-            }
-        return point_value
 
 
 class hfslog_stripper(object):
 
     def __init__(self):
         self.date_string_length = 20
-        self.data_flag_set = list()
-        #set up dictionary of key words we look for in the log:
-        #currently we only care about UDC-C, the data validity on/off and source information
-        self.data_flag_dict = {"#popen#udccc/updown" : 1,  "data_valid": 2, "source=": 3}
-        self.data_flag_list = []
-        for key, val in self.data_flag_dict.items():
-            self.data_flag_list.append(key)
-        
+        self.line_type_tuple = ( udc_status(), antenna_target_status(), source() )
         #temporary storage of a parse data item (list of dicts), only valid if process_line returns true
         self.data_points = []
-
+s
     def process_line(self,line):
-        #quick check that the line is longer
-        #than the required date string format length
+        #quick check that the line is longer than the required date string format length
+        success = False
         self.data_points = []
         if len(line) < self.date_string_length :
-            return False
+            success = False
         else:
-            #determine if we have a line that contains information we are interested in
-            line_code = self.quick_cull(line)
-            if line_code == 1:
-                #process UDC-C line
-                self.process_udc_status( self.extract_udcc_line( line.strip() ) )
-                return True
-            elif line_code == 2:
-                self.process_data_status( self.extract_data_valid_line( line.strip() ) )
-                return True
-            elif line_code == 3:
-                self.process_source_status( self.extract_source_line( line.strip() ) )
-                return True
-            else:
-                return False
+            for line_type in self.line_type_tuple:
+                if line_type.line_key in line:
+                    success = line_type.initialize_from_line(line)
+                    if success is True:
+                        self.data_points = [ line_type.as_dict() ]
+                        break
+                    else:
+                        print("Could not parse line: ", line)
+                        break
+        return success
 
     def get_data_points(self):
         return self.data_points
-
-    #strip the date code from a log line, return time stamp object
-    def strip_date(self, line):
-        date_string = line[0:self.date_string_length]
-        #date string should be formatted like 2018.060.15:05:26.36
-        syear = date_string[0:4]
-        sday = date_string[5:8]
-        shour = date_string[9:11]
-        sminute = date_string[12:14]
-        ssec = date_string[15:17]
-        shundredths = date_string[18:20]
-        ts = time_stamp()
-        ts.year = int(syear)
-        ts.day = int(sday)
-        ts.hour = int(shour)
-        ts.minute = int(sminute)
-        ts.seconds = int(ssec)
-        ts.microseconds = int(shundredths)*10000
-        return ts
-
-    #returns 0 if this line is unrecognized and should be culled, 
-    #otherwise numeric code indicates the line type 
-    def quick_cull(self, line):
-        ret_val = 0
-        for key,val in self.data_flag_dict.items():
-            if key in line:
-                ret_val = val
-                break
-        return ret_val
-
-    def extract_udcc_line(self, line):
-        #we expect the line to have the following format: 2018.061.13:12:04.39#popen#udccc/updown 7083.1 20 20 0 0 status 0
-        stat = udc_status()
-        tokens = line.split()
-        if len(tokens) == 8:
-            if "#popen#udccc/updown" in tokens[0]:
-                stat.parse_valid = True
-                stat.log_time = self.strip_date(line)
-                stat.frequency_MHz = float(tokens[1])
-                stat.atten_h = float(tokens[2])
-                stat.atten_v = float(tokens[3])
-                stat.status_code = int(tokens[7])
-        return stat
-
-    def extract_data_valid_line(self, line):
-        #we expect the line to have the following format: 2018.061.13:11:54.00:data_valid=on 
-        #or alternatively: 2018.061.13:12:24.00:data_valid=off
-        stat = data_status()
-        tokens = line.split('=')
-        if len(tokens) == 2:
-            if "data_valid" in tokens[0]:
-                stat.parse_valid = True
-                stat.log_time = self.strip_date(line)
-                stat.status_code = tokens[1]
-        return stat
-
-    def extract_source_line(self, line):
-        #we expect the line to have the following format: 2018.061.13:11:16.00:source=1741-038,174120.64,-034848.9,1950.0,neutral
-        stat = source_status()
-        tokens = line.split('=')
-        if len(tokens) == 2:
-            if "source" in tokens[0]:
-                tokens2 = tokens[1].split(',')
-                if len(tokens2) == 5:
-                    stat.parse_valid = True
-                    stat.log_time = self.strip_date(line)
-                    stat.source_name = tokens2[0]
-                    stat.ra = tokens2[1]
-                    stat.dec = tokens2[2]
-                    stat.epoch = tokens2[3]
-                    stat.cable_wrap = tokens2[4]
-        return stat
-
-    def process_udc_status(self, udc):
-        if udc.parse_valid is True:
-            self.data_points = [ udc.as_dict() ]
-            # print("process:", udc.as_dict() )
-        else:
-            # print("UDC parse error")
-            self.data_points = []
-
-    def process_data_status(self, data):
-        if data.parse_valid is True:
-            self.data_points = [ data.as_dict() ]
-            # print("process:", data.as_dict() )
-        else:
-            # print("Data validity parse error")
-            self.data_points = []
-
-    def process_source_status(self, source):
-        if source.parse_valid is True:
-            self.data_points = [ source.as_dict() ]
-            # print("process:", source.as_dict() )
-        else:
-            # print("Source parse error")
-            self.data_points = []
