51fc1888527c7f1549272284653b0b6098fad553
John Barrett <barrettj@mit.edu>
2019-06-04 13:58:59 -0400

    change number of threads

--- source/cpp_src/Main/include/HSpectrometerManager.hh
+++ source/cpp_src/Main/include/HSpectrometerManager.hh
@@ -63,7 +63,7 @@ extern "C"
     #define N_SPECTROMETER_POOL_SIZE 16
     #define N_NOISE_POWER_POOL_SIZE 10
     #define N_SPECTROMETER_THREADS 3
-    #define N_NOISE_POWER_THREADS 2
+    #define N_NOISE_POWER_THREADS 1
     #define DUMP_FREQ 120
     #define N_AVE_BUFFERS 12 //this is about once every second
     #define SPEC_AVE_POOL_SIZE 20
@@ -83,7 +83,7 @@ extern "C"
         #define N_SPECTROMETER_POOL_SIZE 4
         #define N_SPECTROMETER_THREADS 1
         #define N_NOISE_POWER_POOL_SIZE 10
-        #define N_NOISE_POWER_THREADS 2
+        #define N_NOISE_POWER_THREADS 1
         #define DUMP_FREQ 2
         #define N_AVE_BUFFERS 2
         #define SPEC_AVE_POOL_SIZE 4
