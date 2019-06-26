a29236b9b9aaa153d3720ca0005d7eabadea064a
John Barrett <barrettj@mit.edu>
2019-06-26 16:07:33 -0400

    reconfigure digitizer to use external 10MHz clock and 1pps trigger

--- source/cpp_src/Devices/src/HADQ7Digitizer.cc
+++ source/cpp_src/Devices/src/HADQ7Digitizer.cc
@@ -9,14 +9,14 @@ namespace hose
 HADQ7Digitizer::HADQ7Digitizer():
     fSidebandFlag('?'),
     fPolarizationFlag('?'),
-    fClockMode(0), //0 is internal 10MHz reference, 1 uses external 10MHz reference
+    fClockMode(1), //0 is internal 10MHz reference, 1 uses external 10MHz reference
     fADQControlUnit(nullptr),
     fADQDeviceNumber(1), //board id
     fSerialNumber(),
     fEnableA(1),
     fEnableB(0),
     fNChannels(0),
-    fUseSoftwareTrigger(true),
+    fUseSoftwareTrigger(false),
     fSampleRate(0),
     fAcquisitionRateMHz(0), //effective sampling frequency in MHz
     fSampleSkipFactor(2),
