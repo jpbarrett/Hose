56865d486c9fbd820de0d66a296678af3810f9a4
John Barrett <barrettj@mit.edu>
2021-07-07 13:10:53 -0400

    add file

---
+++ source/cpp_src/MetaData/include/HParameters.hh
@@ -0,0 +1,48 @@
+#ifndef HParameters_HH__
+#define HParameters_HH__
+
+/*
+*File: HParameters.hh
+*Class: HParameters
+*Author: J. Barrett
+*Email: barrettj@mit.edu
+*Date:
+*Description: class to read/store spectrometer parameters (with reasonable defaults)
+*/
+
+#include <string>
+#include <map>
+
+namespace hose 
+{
+
+class HParameters
+{
+    public:
+        HParameters();
+        virtual ~HParameters();
+
+        //set a filename and read the parameter values
+        void SetParameterFilename(std::string& file);
+        void ReadParameters();
+
+        //reset parameter values to the defaults
+        void UseDefaultParameters();
+
+        int GetIntegerParameter(std::string& name);
+        std::string GetStringParameter(std::string name);
+        
+    private:
+
+        std::string fParameterFile;
+        bool fHaveParameterFile;
+        //integer parameter map
+        std::map<std::string, int> fIntegerParam;
+        //string parameter map
+        std::map<std::string, std::string> fStringParam;
+
+};
+
+}
+
+#endif /* end of include guard: HParameters */
