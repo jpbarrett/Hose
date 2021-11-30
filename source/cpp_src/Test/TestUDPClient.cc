0c3805a3505e607a9598215b0bf79de487f2b433
John Barrett <barrettj@mit.edu>
2021-11-30 11:36:25 -0500

    doh...copypasta

--- source/cpp_src/Test/TestUDPClient.cc
+++ source/cpp_src/Test/TestUDPClient.cc
@@ -8,8 +8,17 @@
 #include <arpa/inet.h>
 #include <netinet/in.h>
 
-//taken from 
-//https://www.geeksforgeeks.org/udp-server-client-implementation-c/
+
+
+// Client side implementation of UDP client-server model
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 #define PORT	 8080
 #define MAXLINE 1024
@@ -18,45 +27,36 @@
 int main() {
 	int sockfd;
 	char buffer[MAXLINE];
-	char *hello = "Hello from server";
-	struct sockaddr_in servaddr, cliaddr;
-	
+	char *hello = "Hello from client";
+	struct sockaddr_in	 servaddr;
+
 	// Creating socket file descriptor
 	if ( (sockfd = socket(AF_INET, SOCK_DGRAM, 0)) < 0 ) {
 		perror("socket creation failed");
 		exit(EXIT_FAILURE);
 	}
-	
+
 	memset(&servaddr, 0, sizeof(servaddr));
-	memset(&cliaddr, 0, sizeof(cliaddr));
 	
 	// Filling server information
-	servaddr.sin_family = AF_INET; // IPv4
-    servaddr.sin_addr.s_addr=inet_addr("192.52.61.185");
-	//servaddr.sin_addr.s_addr = INADDR_ANY;
+	servaddr.sin_family = AF_INET;
 	servaddr.sin_port = htons(PORT);
+    servaddr.sin_addr.s_addr=inet_addr("127.0.0.1");
+//	servaddr.sin_addr.s_addr = INADDR_ANY;
 	
-	// Bind the socket with the server address
-	if ( bind(sockfd, (const struct sockaddr *)&servaddr,
-			sizeof(servaddr)) < 0 )
-	{
-		perror("bind failed");
-		exit(EXIT_FAILURE);
-	}
+	unsigned int n, len;
 	
-	unsigned int len, n;
-
-	len = sizeof(cliaddr); //len is value/resuslt
-
+	sendto(sockfd, (const char *)hello, strlen(hello),
+		MSG_CONFIRM, (const struct sockaddr *) &servaddr,
+			sizeof(servaddr));
+	printf("Hello message sent.\n");
+		
 	n = recvfrom(sockfd, (char *)buffer, MAXLINE,
-				MSG_WAITALL, ( struct sockaddr *) &cliaddr,
+				MSG_WAITALL, (struct sockaddr *) &servaddr,
 				&len);
 	buffer[n] = '\0';
-	printf("Client : %s\n", buffer);
-	sendto(sockfd, (const char *)hello, strlen(hello),
-		MSG_CONFIRM, (const struct sockaddr *) &cliaddr,
-			len);
-	printf("Hello message sent.\n");
-	
+	printf("Server : %s\n", buffer);
+
+	close(sockfd);
 	return 0;
 }
