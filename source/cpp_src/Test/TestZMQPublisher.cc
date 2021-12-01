11c885d600cf3865bab1b88f6ab2cc16c19a42b7
John Barrett <barrettj@mit.edu>
2021-12-01 11:55:18 -0500

    add missing tests

---
+++ source/cpp_src/Test/TestZMQPublisher.cc
@@ -0,0 +1,45 @@
+// Server side implementation of UDP client-server model
+#include <zmq.hpp>
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <sstream>
+#include <thread>
+#include <chrono>
+
+int main() 
+{
+
+    zmq::context_t context(1);
+    zmq::socket_t publisher(context, ZMQ_RADIO);
+    // We need set IP of destination, sad but true
+    publisher.connect("udp://192.52.61.185:8181");
+
+    std::string text;
+    text.reserve(128);
+
+    int number = 0;
+    while(publisher.connected())
+    {
+        std::chrono::microseconds timestamp = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::system_clock::now().time_since_epoch());
+
+        text.clear();
+        text += std::to_string(timestamp.count());
+        text += ";";
+        text += std::to_string(++number);
+
+        zmq::message_t update{text.data(), text.size()};
+        //update.set_group("test");
+        std::cout << "Sending: " << timestamp.count() << " number:" << number << std::endl;
+        publisher.send(update);
+        sleep(1);
+        // std::this_thread::sleep_for(1);
+    }
+
+
+
+
+
+
+}
---
+++ source/cpp_src/Test/TestZMQSubscriber.cc
@@ -0,0 +1,50 @@
+// Server side implementation of UDP client-server model
+#include <zmq.hpp>
+#include <string>
+#include <iostream>
+#include <unistd.h>
+#include <sstream>
+#include <thread>
+#include <chrono>
+
+
+int main() 
+{
+    
+    zmq::context_t context(1);
+    zmq::socket_t subscriber(context, ZMQ_DISH);
+    subscriber.bind("udp://*:8181");
+    //subscriber.join("test");
+
+    int previousNumber = 0;
+    int lostCount = -1;
+
+    while(subscriber.connected())
+    {
+        zmq::message_t update;
+
+        subscriber.recv(&update);
+
+        std::string text(update.data<const char>(), update.size());
+        std::cout << text;
+
+        auto splitPoint = text.find(';');
+        std::string serverTime = std::string(text.substr(0, splitPoint));
+        std::string serverNumber = std::string(text.substr(splitPoint + 1));
+        auto number = std::stoi(serverNumber);
+        if(number != previousNumber + 1)
+        {
+            ++lostCount;
+        }
+        previousNumber = number;
+
+        const auto diff =
+            std::chrono::system_clock::now() -
+            std::chrono::system_clock::time_point{std::chrono::microseconds{std::stoull(serverTime)}};
+
+        // Beautify at: https://github.com/gelldur/common-cpp/blob/master/src/acme/beautify.h
+        std::cout << " ping:"  << "UDP lost: " << lostCount << std::endl;
+    }
+
+
+}
