c14b0e71394645e8a92afe4eb84b713d28c5ea50
John Barrett <barrettj@mit.edu>
2021-12-14 11:15:34 -0500

    add zeroMQ library path to hoseenv.sh

--- cmake/FindZeroMQ.cmake
+++ cmake/FindZeroMQ.cmake
@@ -3,16 +3,15 @@
 # ZMQ_FOUND - System has ZMQ
 # ZMQ_INCLUDE_DIRS - The ZMQ include directories
 # ZMQ_LIBRARIES - The libraries needed to use ZMQ
-# ZMQ_DEFINITIONS - Compiler switches required for using ZMQ
 
 find_path ( ZMQ_INCLUDE_DIR zmq.h )
 find_library ( ZMQ_LIBRARY NAMES zmq )
-#find_library ( ZMQPP_LIBRARY NAMES zmqpp )
+find_path ( ZMQ_LIB_DIR NAMES libzmq )
 
 set ( ZMQ_LIBRARIES ${ZMQ_LIBRARY} )
 set ( ZMQ_INCLUDE_DIRS ${ZMQ_INCLUDE_DIR} )
-
+set ( ZMQ_LIBDIR ${ZMQ_LIB_DIR}b)
 include ( FindPackageHandleStandardArgs )
 # handle the QUIETLY and REQUIRED arguments and set ZMQ_FOUND to TRUE
 # if all listed variables are TRUE
-find_package_handle_standard_args ( ZMQ DEFAULT_MSG ZMQ_LIBRARY ZMQPP_LIBRARY ZMQ_INCLUDE_DIR )
+find_package_handle_standard_args ( ZMQ DEFAULT_MSG ZMQ_LIBRARY ZMQ_INCLUDE_DIR ZMQ_LIBDIR )
