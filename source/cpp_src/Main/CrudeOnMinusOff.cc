32c9eeac51629e9404c3c32adbc956df9799909c
John Barrett <barrettj@mit.edu>
2021-12-21 15:53:35 -0500

    add addition npts config files

--- source/cpp_src/Main/CrudeOnMinusOff.cc
+++ source/cpp_src/Main/CrudeOnMinusOff.cc
@@ -288,11 +288,11 @@ bool ReadDataDirectory(std::string data_dir, bool toggle_diode,  MetaDataContain
     }
 
 
-    if(!map_to_sky_frequency || ! source_info || !recording_status_info)
-    {
-        std::cout<<"Error: missing meta-data!"<<std::endl;
-        return false;
-    }
+    //if(!map_to_sky_frequency || ! source_info || !recording_status_info)
+    //{
+    //    std::cout<<"Error: missing meta-data!"<<std::endl;
+    //    return false;
+    //}
 
 ////////////////////////////////////////////////////////////////////////////////
 //compute an averaged spectrum
