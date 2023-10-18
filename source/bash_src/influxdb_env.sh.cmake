9a93f9ed8d4790ee289fdd7c4a65aba5812758a6
John Barrett <barrettj@mit.edu>
2023-10-18 16:35:21 -0400

    make dummy values obvious

--- source/bash_src/influxdb_env.sh.cmake
+++ source/bash_src/influxdb_env.sh.cmake
@@ -2,12 +2,9 @@
 
 #skelton file to set up environment variables needed to access local
 #influxdb server, dummy_values should be filled in for the local configuration
-#these are the environment variables to access influxdb running
-#on wfmark5-19:
-#TODO, make this more secure!
 
-export INFLUXDB_HOSTNAME="wfmark5-19"
-export INFLUXDB_PORT="8086"
-export INFLUXDB_USER="GPUSpecOper"
-export INFLUXDB_PWD="kLx14f2p"
-export INFLUXDB_DATABASE="gpu_spec"
+export INFLUXDB_HOSTNAME="dummy-host"
+export INFLUXDB_PORT="8000"
+export INFLUXDB_USER="dummy-user"
+export INFLUXDB_PWD="dummy-pwd"
+export INFLUXDB_DATABASE="dummy-db"
